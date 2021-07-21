229c486cb4017074c3017d485a6ef4c52e017629
chaejinims2 <80671370+chaejinims2@users.noreply.github.com>
2021-07-21 16:55:04 +0900

    7/21 4ban class commit 1 Get/Post

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,21 +12,23 @@ from accountapp.models import NewModel
 
 
 def hello_world(request):
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        if request.method == "POST":
 
-        temp = request.POST.get('input_text')
+            temp = request.POST.get('input_text')
 
-        new_model = NewModel()
-        new_model.text = temp
-        new_model.save()
+            new_model = NewModel()
+            new_model.text = temp
+            new_model.save()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
+        else:
+            data_list = NewModel.objects.all()
+            return render(request, 'accountapp/hello_world.html',
+                          context={'data_list': data_list})
     else:
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
@@ -34,10 +36,34 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+    def get(self, request, *args, **kargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def post(self, request, *args, **kargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    def get(self, request, *args, **kargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def post(self, request, *args, **kargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountUpdateView(UpdateView):
     model = User

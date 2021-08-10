7361a2bb593242e2dee1d6a7c64633fe5362a8d6
chaejin <chaejinims2@gmail.com>
2021-08-10 12:14:51 +0900

    8/10 4ban class commit 3

--- commentapp/forms.py
+++ commentapp/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from commentapp.models import Comment
 
 
-class CommentCreationForm(ModelForm):
+class CommentCreateForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['content']

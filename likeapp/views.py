748ff8f0576661ddc6eab1a361054aa4393f6aa3
chaejin <chaejinims2@gmail.com>
2021-08-24 12:22:03 +0900

    8/24 4ban class commit 4

--- likeapp/views.py
+++ likeapp/views.py
@@ -20,13 +20,15 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user,
                                                 article=article)
         if like_record.exists():
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
+            return HttpResponseRedirect(reverse('articleapp:detail',
+                                                kwargs={'pk':kwargs['article_pk']}))
 
         else:
             LikeRecord(user=user, article=article).save()
 
         article.like += 1
         article.save()
-
-
         return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']})

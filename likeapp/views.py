0a954d46047008c044a212a8c18d21a867bbf836
chaejin <chaejinims2@gmail.com>
2021-08-25 16:54:35 +0900

    8/25 4ban class commit 1

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.messages.context_processors import messages
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 

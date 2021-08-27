0afd1ffdd14fa7cf004674f6f7ce17b6236a04c2
chaejin <chaejinims2@gmail.com>
2021-08-27 11:27:55 +0900

    8/27 4ban class commit 1

--- profileapp/views.py
+++ profileapp/views.py
@@ -17,7 +17,6 @@ from profileapp.models import Profile
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -34,7 +33,6 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
 
     def get_success_url(self):
